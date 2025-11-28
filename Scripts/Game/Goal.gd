b077817df168b38e8888292ac16bb74ce4ff3e23
Ángel Villanueva <95488416+844759@users.noreply.github.com>
2025-11-28 20:45:12 +0100

    Fix

--- Scripts/Game/Goal.gd
+++ Scripts/Game/Goal.gd
@@ -1,17 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-<<<<<<< HEAD
-	if not body.is_in_group("Player"):
-		return
-
-	if is_locked:
-		World.HUD.show_hungry_message(2.0)
-		print("Goal is locked. Donuts left:", World.donuts_left)
-		return
-
-	World.game_completed()
-=======
 	if body.is_in_group("Player"):
 		World.game_completed()
->>>>>>> parent of d1eb90c (AllDonuts)
